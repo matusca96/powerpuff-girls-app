792afb66a02887c9d78f0d9b1416cbea5873a43d
matusca96 <gomesloud@gmail.com>
2021-11-06 21:27:30 -0300

    fix: adjust test

--- src/tests/App.spec.tsx
+++ src/tests/App.spec.tsx
@@ -2,7 +2,7 @@ import 'react-native';
 import React from 'react';
 import { render } from '@testing-library/react-native';
 
-import { App } from '../../App';
+import { App } from '../App';
 
 jest.mock('expo', () => ({
   AppLoading: 'AppLoading',
